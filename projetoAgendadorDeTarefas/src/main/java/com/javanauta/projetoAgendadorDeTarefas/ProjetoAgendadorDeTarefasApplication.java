db6d0d971f6532a9d35de6d4de5185ce41bcc1c7
CarlaCristine48 <carlabarbosacristine@gmail.com>
2026-07-19 14:15:30 -0300

    commit inicial

---
+++ projetoAgendadorDeTarefas/src/main/java/com/javanauta/projetoAgendadorDeTarefas/ProjetoAgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.projetoAgendadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAgendadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ projetoAgendadorDeTarefas/src/test/java/com/javanauta/projetoAgendadorDeTarefas/ProjetoAgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.projetoAgendadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
